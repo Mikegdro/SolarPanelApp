1f6d79663fef38003cb46c36cbb5590cb953cd33
Sergio Parejo <sergioparejo671@gmail.com>
2023-02-25 20:21:24 +0100

    WebSocket Server Events, Implementing redis for induvidual client messages

--- ws/build/server.js
+++ ws/build/server.js
@@ -1,5 +1,60 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var fs = require('fs');
+var axios = require('axios');
+var redis = require("redis");
+var jwt = require('jsonwebtoken');
+var client = redis.createClient({
+    url: 'redis://localhost:6379' // Cambiar localhost por nombre contenedor Docker
+});
+function connectRedis() {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, client.connect()];
+                case 1:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+connectRedis();
 require('dotenv').config();
 var io = require("socket.io")(3000, {
     cors: {
@@ -9,51 +64,84 @@ var io = require("socket.io")(3000, {
 var oneDay = 86400000;
 var interval = 7200000;
 var start = Date.now();
-setInterval(function () {
-    var now = Date.now();
-    var elapsed = now - start;
-    if (elapsed >= oneDay) {
-        start = Date.now();
-        console.log(process.env.PRIVATE_KEY);
-        // Fetch a ruta de api para volcar los datos en la bd
-    }
-}, interval);
+// Intervalo para que cada 24 horas de dumpee el json a la db
+setInterval(function () { return __awaiter(void 0, void 0, void 0, function () {
+    var now, elapsed;
+    return __generator(this, function (_a) {
+        now = Date.now();
+        elapsed = now - start;
+        if (elapsed >= oneDay) {
+            start = Date.now();
+            axios.post("http://laravel:8000/api/dump", getLogs())
+                .catch(function (e) {
+                console.log(e.message);
+            })
+                .then(function () {
+                console.log("Data Dumped: " + new Date(Date.now()));
+            });
+        }
+        return [2 /*return*/];
+    });
+}); }, interval);
 function authMiddleware(socket, next) {
-    if (socket.handshake.auth.token) {
-        var isAuth = true;
-        // let isAuth = fetch(api route de comprobar token);
-        if (!isAuth) {
-            next(new Error("The token provided is not valid"));
-        }
-        next();
-    }
-    else if (socket.handshake.auth.id) {
-        var isAuth = true;
-        // let isAuth = fetch(api route de comprobar id del panel en whitelist);
-        if (!isAuth) {
-            next(new Error("The panel id provided is not registered as valid"));
-        }
-        next();
-    }
-    else {
-        next(new Error("You need to authenticate to establish a connection"));
-    }
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            if (socket.handshake.auth.token) {
+                try {
+                    // jwt.verify(socket.handshake.auth.token, process.env.PRIVATE_KEY); // Comprobamos si el token ha sido firmado por nosotros - Descomentar en producción
+                    next();
+                }
+                catch (e) {
+                    next(new Error("The token provided is not valid"));
+                }
+            }
+            else {
+                next(new Error("You need to authenticate to establish a connection"));
+            }
+            return [2 /*return*/];
+        });
+    });
 }
 io.use(authMiddleware);
 io.on('connection', function (socket) {
     console.log("socket connected: " + socket);
     socket.on('solar-panel-update', function (data) {
         if (instanceOfPanelUpdate(data)) {
-            // insertLog(data);
+            insertLog(data);
             io.emit('panel-update', data);
-            io.emit('solar-panel-photo', "hola");
         }
     });
-    socket.on('solar-panel-photo', function (data) {
-        console.log("HA LLEGADO: ");
+    socket.on('save-panel-id', function (data) {
         console.log(data);
-        // insertLog(data);
+        client.set(data.id, socket.id);
     });
+    socket.on('solar-panel-command', function (data) { return __awaiter(void 0, void 0, void 0, function () {
+        var socketId;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, client.get(data.id)];
+                case 1:
+                    socketId = _a.sent();
+                    console.log(socketId);
+                    io.to(socketId).emit('command', {
+                        command: data.command,
+                    });
+                    return [2 /*return*/];
+            }
+        });
+    }); });
+    socket.on('manual-data-dump', function () { return __awaiter(void 0, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            axios.post("http://laravel:8000/api/dump", getLogs())
+                .then(function () {
+                console.log("Data Dumped: " + new Date(Date.now()));
+            })
+                .catch(function (e) {
+                console.log(e.message);
+            });
+            return [2 /*return*/];
+        });
+    }); });
 });
 function insertLog(data) {
     var file = fs.readFileSync('logs.json');
@@ -65,29 +153,17 @@ function insertLog(data) {
         fileData[data.id] = [data];
     }
     var newData = JSON.stringify(fileData);
-    fs.writeFile('logs.json', newData, function (err) {
-        // error checking
+    fs.writeFileSync('logs.json', newData, function (err) {
         if (err)
             throw err;
-        // console.log("New data added");
+        console.log("New data added");
     });
 }
-insertLog({
-    id: "panel2",
-    time: "234",
-    log: {
-        sensor1: 12,
-        sensor2: 32,
-        sensor3: 52,
-        sensor4: 52,
-        motor1: 52,
-        motor2: 23,
-        battery: "6235",
-        potency: 23,
-        image: "ddadasqwdadawadasd",
-        ocvOutput: "ddadasqwdadawadasd"
-    }
-});
 function instanceOfPanelUpdate(object) {
-    return 'panelId' in object;
+    return 'id' in object;
+}
+function getLogs() {
+    var file = fs.readFileSync('logs.json');
+    var fileData = JSON.parse(file);
+    return fileData;
 }
--- ws/src/server.ts
+++ ws/src/server.ts
@@ -1,40 +1,75 @@
 const fs = require('fs');
+const axios = require('axios');
+const redis = require("redis");
+const jwt = require('jsonwebtoken');
+
+const client = redis.createClient({
+    url: 'redis://localhost:6379' // Cambiar localhost por nombre contenedor Docker
+});
+async function connectRedis(){
+    await client.connect()
+}
+connectRedis();
+
 require('dotenv').config();
 const io = require("socket.io")(3000, {
     cors: {
         origin: ["http://localhost:8080"], // Aquí se pondrán el/los clientes que se conecten al ws
     },
 });
+
 const oneDay = 86400000;
 const interval = 7200000;
 let start = Date.now();
 
-setInterval(() =>{
+// Modelos de mensajes
+interface PanelUpdate {
+    id: string// Identificador único del panel
+    time: string // Fecha del update
+    type: string
+    log: {
+        sensor1: number
+        sensor2: number
+        sensor3: number
+        sensor4: number
+        motor1: number
+        motor2: number
+        battery: string
+        potency: number
+        image: string
+        ocvOutput: string
+    }
+}
+
+interface PanelCommand {
+    id: string
+    command: string
+}
+
+// Intervalo para que cada 24 horas de dumpee el json a la db
+setInterval(async () =>{
     let now = Date.now();
     let elapsed = now - start;
     if(elapsed >= oneDay){
         start = Date.now();
-        console.log(process.env.PRIVATE_KEY);
-        // Fetch a ruta de api para volcar los datos en la bd
+        axios.post("http://laravel:8000/api/dump", getLogs())
+            .catch((e : any) =>{
+                console.log(e.message)
+            })
+            .then(() =>{
+                console.log("Data Dumped: " + new Date(Date.now()));
+            });
     }
 }, interval)
 
-
-function authMiddleware(socket : any, next : Function){
+async function authMiddleware(socket : any, next : Function){
     if(socket.handshake.auth.token){
-        let isAuth = true;
-        // let isAuth = fetch(api route de comprobar token);
-        if(!isAuth){
+        try{
+            // jwt.verify(socket.handshake.auth.token, process.env.PRIVATE_KEY); // Comprobamos si el token ha sido firmado por nosotros - Descomentar en producción
+            next();
+        }catch(e){
             next(new Error("The token provided is not valid"));
         }
-        next();
-    }else if(socket.handshake.auth.id){
-        let isAuth = true;
-        // let isAuth = fetch(api route de comprobar id del panel en whitelist);
-        if(!isAuth){
-            next(new Error("The panel id provided is not registered as valid"));
-        }
-        next();
     }else{
         next(new Error("You need to authenticate to establish a connection"));
     }
@@ -46,19 +81,34 @@ io.on('connection', (socket : any) =>{
     
     socket.on('solar-panel-update', (data : PanelUpdate) =>{
         if(instanceOfPanelUpdate(data)){
-            // insertLog(data);
+            insertLog(data);
             io.emit('panel-update', data);
-            io.emit('solar-panel-photo', "hola");
         }
     });
 
-    socket.on('solar-panel-photo', (data : any) =>{
-        console.log("HA LLEGADO: ");
-        console.log(data);
-        // insertLog(data);
-    })
-})
+    socket.on('save-panel-id', (data : any) =>{
+        console.log(data)
+        client.set(data.id, socket.id);
+    });
 
+    socket.on('solar-panel-command', async (data : PanelCommand) =>{
+        let socketId = await client.get(data.id);
+        console.log(socketId)
+        io.to(socketId).emit('command', {
+            command: data.command,
+        });
+    });
+
+    socket.on('manual-data-dump', async () =>{
+        axios.post("http://laravel:8000/api/dump", getLogs())
+        .then(() =>{
+            console.log("Data Dumped: " + new Date(Date.now()));
+        })
+        .catch((e : any) =>{
+            console.log(e.message)
+        });
+    });
+})
 
 function insertLog(data : PanelUpdate){
     let file = fs.readFileSync('logs.json');
@@ -69,47 +119,19 @@ function insertLog(data : PanelUpdate){
         fileData[data.id] = [data];
     }
     let newData = JSON.stringify(fileData);
-    fs.writeFile('logs.json', newData, (err : any) => {
-        // error checking
+    fs.writeFileSync('logs.json', newData, (err : any) => {
         if(err) throw err;
         
-        // console.log("New data added");
+        console.log("New data added");
     });  
-}
-insertLog({
-    id: "panel2",
-    time: "234",
-    log: {
-        sensor1: 12,
-        sensor2: 32,
-        sensor3: 52,
-        sensor4: 52,
-        motor1: 52,
-        motor2: 23,
-        battery: "6235", // %
-        potency: 23, // Maybe?
-        image: "ddadasqwdadawadasd", // base64???
-        ocvOutput: "ddadasqwdadawadasd"
-    }
-})
-
-interface PanelUpdate {
-    id: string// Identificador único del panel
-    time: string // Fecha del update
-    log: {
-        sensor1: number
-        sensor2: number
-        sensor3: number
-        sensor4: number
-        motor1: number
-        motor2: number
-        battery: string // %
-        potency: number // Maybe?
-        image: string // base64???
-        ocvOutput: string
-    }
 }
 
 function instanceOfPanelUpdate(object: any): object is PanelUpdate {
-    return 'panelId' in object;
+    return 'id' in object;
+}
+
+function getLogs(){
+    let file = fs.readFileSync('logs.json');
+    let fileData = JSON.parse(file);
+    return fileData;
 }
